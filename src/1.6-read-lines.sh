dbbfac2de508609a65ecf13555207f1e83489a4e
ABHISHEK MAURYA <118133017+abhishekmaurya000786@users.noreply.github.com>
2025-02-02 21:50:25 +0000

    4th change

--- src/1.6-read-lines.sh
+++ src/1.6-read-lines.sh
@@ -3,3 +3,29 @@
 #Task: Display the first three lines of the file
 #Task: Display the last three lines of the same file
 
+cat > 10_lines.txt <<EOF
+This is line 1.
+This is line 2.
+This is line 3.
+This is line 4.
+This is line 5.
+This is line 6.
+This is line 7.
+This is line 8.
+This is line 9.
+This is line 10.
+EOF
+cat >> 10_lines.txt <<EOF
+This is line 1.
+This is line 2.
+This is line 3.
+This is line 4.
+This is line 5.
+This is line 6.
+This is line 7.
+This is line 8.
+This is line 9.
+This is line 10.
+EOF
+
+tail -n 3 10_lines.txt
